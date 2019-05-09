1b567c1f5390a6f2a2e167b58e85e6a117d8df29
Anna <anna.scibska@campusid.eu>
2019-05-09 17:31:48 +0200

    first commit

---
+++ xamarin1/xamarin1/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace xamarin1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=b67aa439-bb4b-4292-b83c-44f2bf6b1af5;"
+                + "ios=78dd993c-a711-47b2-b741-b5665200d070;" 
+                + "uwp=b393e627-7e88-4571-a1ac-d69a65cbe71f", 
+                typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ xamarin1/xamarin1/MainPage.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarin1
+{
+
+    public class Post
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+
+        public int id { get; set; }
+        public string title { get; set; }
+        public ObservableCollection<Post> Data { get; set; } = new ObservableCollection<Post>();
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            this.Data = Data;
+            request(Data);
+        }
+
+        public void Button_Clicked(object sender, EventArgs e)
+        {
+            var p = new Post { id = 5, title = title };
+            Data.Add(p);
+        }
+
+        public async static void request(ObservableCollection<Post> Data)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://my-json-server.typicode.com/typicode/demo/posts");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var data = Newtonsoft.Json.JsonConvert.DeserializeObject<ObservableCollection<Post>>(json);
+                foreach(Post d in data)
+                {
+                    Data.Add(d);
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //    // public string Text { get; set; } = "dupa"; // solution 1
+
+        //    public List<string> Data { get; set; }
+        //    public ObservableCollection<List<string>> List { get; set; }
+
+        //    private string _text = "text to change"; // solution 2 beginning
+        //    public string Text
+        //    {
+        //        get => _text;
+        //        set
+        //        {
+        //            if(_text != value)
+        //            {
+        //                _text = value;
+        //                OnPropertyChanged(nameof(Text));
+        //            }
+        //        }
+        //    } // solution 2 end
+
+        //    public MainPage()
+        //    {
+        //        InitializeComponent();
+        //        Data = new List<String>
+        //        {
+        //            "I",
+        //            "hate",
+        //            "C#"
+        //        };
+        //        List = new ObservableCollection<List<string>>
+        //        {
+        //            Data
+        //        };
+        //        List.CollectionChanged += OnListChanged;
+        //        BindingContext = this; // one to the whole Page 
+        //    }
+
+        //    private void OnListChanged(object sender, NotifyCollectionChangedEventArgs args)
+        //    {
+        //        DisplayAlert("Info", "List changed", "OK");
+        //    }
+
+        //    private void Button_Clicked(object sender, EventArgs e)
+        //    {
+        //        Debug.WriteLine("click");
+        //        Text = "Don't touch that!";
+        //        // OnPropertyChanged("Text");  //  OnPropertyChanged(nameOf(Text)); // pierwsze stare, drugie nowe // Solution 1
+        //        Data.RemoveAt(0);
+        //    }
+
+        //    private void Say_Hello_Clicked(object sender, EventArgs e)
+        //    {
+        //        DisplayAlert("Hello", "Anula's App", "OK");
+        //    }
+        //}
+    }
